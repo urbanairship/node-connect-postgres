16e7fa25bd24abf085e6ec8205e9dd6f01600396
AWinterman <andrew.winterman@urbanairsip.com>
2018-10-30 10:06:51 -0700

    apply prettier

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-var connect = require('urban-airship-connect')
-var postgres = require('./postgres')
-var xtend = require('xtend')
-var winston = require('winston')
+var connect = require("urban-airship-connect");
+var postgres = require("./postgres");
+var xtend = require("xtend");
+var winston = require("winston");
 
-winston.level = process.env.LOG_LEVEL || "debug"
+winston.level = process.env.LOG_LEVEL || "debug";
 winston.configure({
-  transports: [new winston.transports.Console({level: winston.level})]
-})
+  transports: [new winston.transports.Console({ level: winston.level })]
+});
 
+process.on("unhandledRejection", onError);
 
-process.on('unhandledRejection', onError)
-
-var app = process.env.UA_APP_KEY
-var token = process.env.UA_CONNECT_TOKEN
+var app = process.env.UA_APP_KEY;
+var token = process.env.UA_CONNECT_TOKEN;
 
 if (!app) {
   winston.error("No app specified, expected env variable: UA_APP_KEY");
-  process.exit(1)
+  process.exit(1);
 }
 
 if (!token) {
   winston.error("No app specified, expected env variable: UA_CONNECT_TOKEN");
-  process.exit(1)
+  process.exit(1);
 }
 
 var config = {
@@ -30,42 +29,49 @@ var config = {
   database: process.env.DB_NAME,
   host: process.env.PGHOST,
   port: process.env.PGPORT,
-  query: process.env.UA_CONNECT_QUERY || {start: "EARLIEST"}, 
+  query: process.env.UA_CONNECT_QUERY || { start: "EARLIEST" },
   app: app,
   token: token,
   logLevel: winston.level,
   sampleIntervalMillis: process.env.SAMPLE_INTERVAL_MILLIS || 1000 * 10
-}
+};
 
-var connectStream = connect(app, token)
+var connectStream = connect(
+  app,
+  token
+);
 
-winston.log('info', 'config', xtend(config, {password: 'redacted', token: 'redacted'}))
+winston.log(
+  "info",
+  "config",
+  xtend(config, { password: "redacted", token: "redacted" })
+);
 
-var postgresStream = postgres(app, config, function (err, streams) { 
+var postgresStream = postgres(app, config, function(err, streams) {
   if (err) {
-    winston.error(err)
-    throw err
+    winston.error(err);
+    throw err;
   }
 
-  streams.start()
+  streams
+    .start()
     .pipe(connectStream)
-      .once('data', (data) => { winston.info('successfully connected to stream; consuming')})
-      .once('data', logAndDelayAddLogger)
+    .once("data", data => {
+      winston.info("successfully connected to stream; consuming");
+    })
+    .once("data", logAndDelayAddLogger)
 
-    .pipe(streams.storage())
+    .pipe(streams.storage());
 
   function logAndDelayAddLogger(data) {
-    winston.debug({"sample event from stream": data})
+    winston.debug({ "sample event from stream": data });
     setTimeout(() => {
-      connectStream.once('data', logAndDelayAddLogger)
-    }, config.sampleIntervalMillis)
+      connectStream.once("data", logAndDelayAddLogger);
+    }, config.sampleIntervalMillis);
   }
-})
-
-
-
+});
 
 function onError(e) {
-    winston.error(e.message, e.stack, e)
-    process.exit(1)
+  winston.error(e.message, e.stack, e);
+  process.exit(1);
 }
